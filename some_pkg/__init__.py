c387af42344882791fc5c48e00ae313114025f22
githubfork123 <117454273+githubfork123@users.noreply.github.com>
2025-06-16 15:45:31 +0000

    some changes

---
+++ some_pkg/__init__.py
@@ -0,0 +1,2 @@
+from .some_pkg import random_func, another_func, yet_another_func
+__all__ = ['random_func', 'another_func', 'yet_another_func']
---
+++ some_pkg/some_pkg/pkg.py
@@ -0,0 +1,6 @@
+def random_func():
+  return 4
+def another_func():
+  return 5
+def yet_another_func():
+  return 6
